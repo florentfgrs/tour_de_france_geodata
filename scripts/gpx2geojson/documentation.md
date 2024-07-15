717bb347d783ef3b5bf17b2b595102a2e186aa3b
Florent Fougères <florent.fougeres@oslandia.com>
2024-07-15 20:01:49 +0200

    Reeprise du script gep2geojson pour l'éxécuter en lot sur un dossier

--- scripts/gpx2geojson/documentation.md
+++ scripts/gpx2geojson/documentation.md
@@ -7,8 +7,16 @@ Le script sert à convertir un fichier GPX en un fichier GeoJSON.
 ## Utilisation
 
 1. Installer les dépendances du script
-2. Éxécuter le script en passant en argument le chemin vers le gpx. 
+2. Éxécuter le script en passant en argument le chemin vers le gpx (ou un dossier contenant des gpx pour uné éxécution en lot).
+
+Pour un gpx :
 
 ```sh
 python gpx2geojson.py path/to/my_trace.gpx
+```
+
+Pour tous les gpx d'un dossier :
+
+```sh
+python gpx2geojson.py path/to/my_folder
 ```
--- scripts/gpx2geojson/gpx2geojson.py
+++ scripts/gpx2geojson/gpx2geojson.py
@@ -1,6 +1,7 @@
 import argparse
 import gpxpy
 import geojson
+from pathlib import Path
 
 def gpx_to_geojson(gpx_file_path):
     with open(gpx_file_path, 'r') as gpx_file:
@@ -20,15 +21,31 @@ def gpx_to_geojson(gpx_file_path):
 
         feature_collection = geojson.FeatureCollection([feature])
 
-        with open(gpx_file_path.replace(".gpx", ".geojson"), 'w') as geojson_file:
+        output_name = str(gpx_file_path).replace(".gpx", ".geojson")
+
+        with open(output_name, 'w') as geojson_file:
             geojson.dump(feature_collection, geojson_file, indent=2)
 
+def process_directory(directory_path):
+    folder_path = Path(directory_path)
+    if folder_path.is_dir():
+        for file_path in folder_path.iterdir():
+            if file_path.is_file() and file_path.suffix.lower() == '.gpx':
+                print(f"Processing file: {file_path}")
+                gpx_to_geojson(file_path)
+
 def main():
-    parser = argparse.ArgumentParser(description="Convertir un fichier GPX en fichier GeoJSON.")
-    parser.add_argument('gpx_file', help="Chemin du fichier GPX à convertir")
+    parser = argparse.ArgumentParser(description="Convertir un fichier GPX ou tous les fichiers GPX d'un dossier en fichiers GeoJSON.")
+    parser.add_argument('path', help="Chemin du fichier GPX ou du dossier contenant les fichiers GPX à convertir")
     args = parser.parse_args()
 
-    gpx_to_geojson(args.gpx_file)
+    path = Path(args.path)
+    if path.is_file():
+        gpx_to_geojson(path)
+    elif path.is_dir():
+        process_directory(path)
+    else:
+        print(f"Le chemin {path} n'est ni un fichier ni un dossier valide.")
 
 if __name__ == "__main__":
     main()
